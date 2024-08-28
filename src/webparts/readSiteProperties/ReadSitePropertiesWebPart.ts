abc84ee53ce20455d466290c4687b89d2157f57c
rezmes <rezmes@gmail.com>
2024-08-28 16:07:26 +0330

    PropertiesPan and start an invoice

--- src/webparts/readSiteProperties/ReadSitePropertiesWebPart.ts
+++ src/webparts/readSiteProperties/ReadSitePropertiesWebPart.ts
@@ -4,7 +4,7 @@ import {
   IPropertyPaneConfiguration,
   PropertyPaneTextField,
 } from "@microsoft/sp-webpart-base";
-import { escape } from "@microsoft/sp-lodash-subset";
+import { escape, truncate } from "@microsoft/sp-lodash-subset";
 
 import styles from "./ReadSitePropertiesWebPart.module.scss";
 import * as strings from "ReadSitePropertiesWebPartStrings";
@@ -13,8 +13,15 @@ import { Environment, EnvironmentType } from "@microsoft/sp-core-library";
 import { SPHttpClient, SPHttpClientResponse } from "@microsoft/sp-http";
 
 export interface IReadSitePropertiesWebPartProps {
-  description: string;
-  environmenttitle: string;
+description: string;
+
+  productname: string;
+  productdescription: string;
+  productcost: number;
+  quantity: number;
+  billamount: number;
+  discount: number;
+  netbillamount: number;
 }
 
 export interface ISharePointList {
@@ -23,75 +30,49 @@ export interface ISharePointList {
 }
 
 export interface ISharePointLists {
-value: ISharePointList[];
+  value: ISharePointList[];
 }
 
-
-
 export default class ReadSitePropertiesWebPart extends BaseClientSideWebPart<IReadSitePropertiesWebPartProps> {
-
-// filter=Hidden eq false instead of select=Title,Id
-private _getListsOfLists(): Promise<ISharePointLists> {
-  return this.context.spHttpClient.get(
-      `${this.context.pageContext.web.absoluteUrl}/_api/web/lists?$filter=Hidden eq false`,
-      SPHttpClient.configurations.v1
-    ).then((response: SPHttpClientResponse) => {
-      // console.log("Response::::::::",response.json());
-      return response.json();
-
-    });
-
+  // filter=Hidden eq false instead of select=Title,Id
+  private _getListsOfLists(): Promise<ISharePointLists> {
+    return this.context.spHttpClient
+      .get(
+        `${this.context.pageContext.web.absoluteUrl}/_api/web/lists?$filter=Hidden eq false`,
+        SPHttpClient.configurations.v1
+      )
+      .then((response: SPHttpClientResponse) => {
+        return response.json();
+      });
   }
 
-
   private _getAndRenderLists(): void {
-    if(Environment.type ===EnvironmentType.Local){
-
-    }
-    else if (
+    if (Environment.type === EnvironmentType.Local) {
+    } else if (
       Environment.type == EnvironmentType.SharePoint ||
-       Environment.type == EnvironmentType.ClassicSharePoint
-      ){
-      this._getListsOfLists()
-        .then((response)=>{
-           this._renderListOfLists(response.value)
+      Environment.type == EnvironmentType.ClassicSharePoint
+    ) {
+      this._getListsOfLists().then((response) => {
+        this._renderListOfLists(response.value);
       });
     }
   }
-private _renderListOfLists(items: ISharePointList[]):void {
-  let html: string = "";
-  items.forEach((item: ISharePointList) => {
-    html += `
+  private _renderListOfLists(items: ISharePointList[]): void {
+    let html: string = "";
+    items.forEach((item: ISharePointList) => {
+      html += `
       <ul class="${styles.list}">
         <li class="${styles.listItem}"><span class="ms-font-1">${item.Title}</span></li>
         <li class="${styles.listItem}"><span class="ms-font-1">${item.Id}</span></li>
       </ul>
     `;
-  });
-  const listsContainer: Element = this.domElement.querySelector(
-    "#spListContainer"
-  );
-  listsContainer.innerHTML = html;
-}
-
-
-
-
-
-
-
-
-  private _findOutEnvironment(): void {
-    //Local environment
-    if (Environment.type === EnvironmentType.Local) {
-      this.properties.environmenttitle = "Local SharePoint Environment";
-    } else if (
-      Environment.type === EnvironmentType.SharePoint ||
-      Environment.type === EnvironmentType.ClassicSharePoint
-    ) {
-      this.properties.environmenttitle = "Online SharePoint Environment";
-    }
+    });
+    const listsContainer: Element =
+      this.domElement.querySelector("#spListContainer");
+    listsContainer.innerHTML = html;
   }
+
+
 
   public render(): void {
     this.domElement.innerHTML = `
@@ -99,13 +80,9 @@ private _renderListOfLists(items: ISharePointList[]):void {
         <div class="${styles.container}">
           <div class="${styles.row}">
             <div class="${styles.column}">
-              <span class="${styles.title}">Welcome to SharePoint!</span>
-              <p class="${
-                styles.subTitle
-              }">Customize SharePoint experiences using Web Parts.</p>
-              <p class="${styles.description}">${escape(
-      this.properties.description
-    )}</p>
+
+
+
 
               <p class="${styles.description}">absolute URL ${escape(
       this.context.pageContext.web.absoluteUrl
@@ -123,9 +100,7 @@ private _renderListOfLists(items: ISharePointList[]):void {
               <p class="${styles.description}">Environment ${
       Environment.type
     }</p>
-              <p class="${styles.description}">Environment ${escape(
-      this.properties.environmenttitle
-    )}</p>
+
 
       <ul>
         <li><strong>curent Culture Name</strong>: ${escape(
@@ -140,40 +115,110 @@ private _renderListOfLists(items: ISharePointList[]):void {
 
       </ul>
 
+        <table>
+          <tr>
+            <td><strong>Product Name</strong></td>
+            <td><strong>${this.properties.productname}</td>
+            </tr>
+            <tr>
+            <td><strong>Description</strong></td>
+            <td>${this.properties.productdescription}</td>
+            </tr>
+            <tr>
+            <td><strong>Product Cost</strong></td>
+            <td>${this.properties.productcost}</td>
+            </tr>
+            <td><strong>Product Quantity</strong></td>
+            <td>${this.properties.quantity}</td>
+
+            <tr>
+            <td>Bill Amount</td>
+            <td>${(this.properties.billamount =
+              this.properties.productcost * this.properties.quantity)}</td>
+            </tr>
+            <tr>
+            <td>Discount</td>
+            <td>${(this.properties.discount =
+              this.properties.billamount * 0.1)}</td>
+            </tr>
+            <tr>
+            <td>Net Bill Amount</td>
+            <td>${(this.properties.netbillamount =
+              this.properties.billamount - this.properties.discount)}</td>
+            </tr>
+            </table>
 
 
 
-
-
-              <a href="https://aka.ms/spfx" class="${styles.button}">
-                <span class="${styles.label}">Learn more</span>
-              </a>
             </div>
           </div>
         </div>
         <div Id="spListContainer"/>
       </div>`;
-        this._getAndRenderLists();
-    this._findOutEnvironment();
+    this._getAndRenderLists();
+
   }
 
-  protected get dataVersion(): Version {
-    return Version.parse("1.0");
-  }
+  // protected get dataVersion(): Version {
+  //   return Version.parse("1.0");
+  // }
+
+  // protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+  //   return {
+  //     pages: [
+  //       {
+  //         header: {
+  //           description: strings.PropertyPaneDescription,
+  //         },
+  //         groups: [
+  //           {
+  //             groupName: strings.BasicGroupName,
+  //             groupFields: [
+  //               PropertyPaneTextField("description", {
+  //                 label: strings.DescriptionFieldLabel,
+  //               }),
+  //             ],
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   };
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
     return {
       pages: [
         {
-          header: {
-            description: strings.PropertyPaneDescription,
-          },
           groups: [
             {
-              groupName: strings.BasicGroupName,
+              groupName: "Product Details",
               groupFields: [
-                PropertyPaneTextField("description", {
-                  label: strings.DescriptionFieldLabel,
+                PropertyPaneTextField("productname", {
+                  label: "Product Name",
+                  multiline: false,
+                  resizable: false,
+                  deferredValidationTime: 5000,
+                  placeholder: "please enter product name", "description": "Name property filed",
+                }),
+                PropertyPaneTextField("productdescription", {
+                  label: "Product Description",
+                  multiline: true,
+                  resizable: false,
+                  deferredValidationTime: 5000,
+                  placeholder: "please enter product Description",  "description": "description property filed",
+                }),
+                PropertyPaneTextField("productcost", {
+                  label: "Product Cost",
+                  multiline: false,
+                  resizable: false,
+                  deferredValidationTime: 5000,
+                  placeholder: "please enter product Cost",  "description": "Number property filed",
+                }),
+                PropertyPaneTextField("quantity", {
+                  label: "Product Quantity",
+                  multiline: false,
+                  resizable: false,
+                  deferredValidationTime: 5000,
+                  placeholder: "please enter product Quantity", "description": "Number property filed",
                 }),
               ],
             },
